5ae3c67d37b1929f7cdea912b375c5fe81c05e85
Amir Ashkan Baghdoust <baghdoust@gmail.com>
2017-02-23 05:15:41 +0100

    gitignore ,grunt and package file updated

--- HTML5Application/Gruntfile.js
+++ HTML5Application/Gruntfile.js
@@ -6,5 +6,40 @@
 module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
+          pkg: grunt.file.readJSON("package.json"),
+            name: "<%= pkg.name %>.<%= pkg.version %>",
+           
+            
+            copy: {
+              nodeModulesJsToLib: {
+                    files: [
+                        {expand: true,
+                            src: [
+                                "node_modules/jquery/dist/jquery.js",
+                                "node_modules/geotiff/dist/geotiff.js",
+                                "node_modules/proj4/dist/proj4.js",
+                               ],
+                               
+                            dest: "public_html/js/",
+                            flatten: true,
+                            filter: "isFile"
+                        }]} ,
+                        
+                cesiumModule: {
+                    files: [
+                     {
+                     cwd:  "node_modules/cesium/dist/",  // set working folder / root to copy
+                    src: '**/*',           // copy all files and subfolders
+                dest: '"public_html/ThirdParty',    // destination folder
+                expand: true  
+                     }]
+                }        
+                        
+    }         
+    
+        
+        
     });
+       require('load-grunt-tasks')(grunt);
+       grunt.registerTask("build", ["copy:nodeModulesJsToLib", "copy:cesiumModule" ]);
 };
